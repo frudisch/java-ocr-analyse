3d7e5a013849e29aaf7c1740e5cc2115559cccdc
Benedikt Linke <benedikt.linke@me.com>
2015-12-05 15:57:07 +0100

    [TASK] add rotate function

--- src/main/java/preprocessing/RotatePreProcessor.java
+++ src/main/java/preprocessing/RotatePreProcessor.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by florian on 23.11.15.
+ * Created by Benedikt Linke on 23.11.15.
  */
 public class RotatePreProcessor extends PreProcessor {
 
@@ -16,14 +16,29 @@ public class RotatePreProcessor extends PreProcessor {
     }
 
     @Override
-    public BufferedImage process(BufferedImage image) {
-        return image;
+    public BufferedImage process(BufferedImage img) {
+        double angle = getValue();
+        double sin = Math.abs(Math.sin(Math.toRadians(angle)));
+        double cos = Math.abs(Math.cos(Math.toRadians(angle)));
+
+        int imgWidth = img.getWidth(null);
+        int imgHeight = img.getHeight(null);
+        int newWidth = (int) Math.floor(imgWidth * cos + imgHeight * sin);
+        int newHeigth = (int) Math.floor(imgHeight * cos + imgWidth * sin);
+
+        BufferedImage newImage = new BufferedImage(newWidth, newHeigth, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = newImage.createGraphics();
+        g.translate((newWidth - imgWidth) / 2, (newHeigth - imgHeight) / 2);
+        g.rotate(Math.toRadians(angle), imgWidth / 2, imgHeight / 2);
+        g.drawRenderedImage(img, null);
+        g.dispose();
+        return newImage;
     }
 
     public static void main(String[] args){
         PreProcessor test = PreProcessingType.ROTATE;
 
-        test.setValue(10.0);
+        test.setValue(18.0);
 
         try {
             BufferedImage rc = test.process(ImageIO.read(new File("./src/main/resources/test_files/Wikipedia_Test_Artikel.PNG")));
