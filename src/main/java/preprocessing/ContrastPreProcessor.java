37d1da439b3278fc3529dd27d99263e4d63b705a
Benedikt Linke <benedikt.linke@me.com>
2015-12-06 12:18:14 +0100

    [TASK] add Contrast and GrayScale

--- src/main/java/preprocessing/ContrastPreProcessor.java
+++ src/main/java/preprocessing/ContrastPreProcessor.java
@@ -2,11 +2,10 @@ package preprocessing;
 
 import ij.ImagePlus;
 import ij.plugin.ContrastEnhancer;
+import ij.process.ColorProcessor;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
 
@@ -21,9 +20,7 @@ public class ContrastPreProcessor extends PreProcessor {
 
     @Override
     public BufferedImage process(BufferedImage image) {
-
-
-        ImagePlus im = new ImagePlus("./src/main/resources/test_files/00 USA  ROADTRIP.png");
+        ImagePlus im = new ImagePlus("Image", image);
         ContrastEnhancer enh = new ContrastEnhancer();
         enh.stretchHistogram(im, getValue());
         return im.getBufferedImage();
@@ -34,10 +31,10 @@ public class ContrastPreProcessor extends PreProcessor {
     public static void main(String[] args){
         PreProcessor test = PreProcessingType.INCREASE_CONTRAST;
 
-        test.setValue(99.0);
+        test.setValue(50.0);
 
         try {
-            BufferedImage rc = test.process(ImageIO.read(new File("./src/main/resources/test_files/Wikipedia_Test_Artikel.PNG")));
+            BufferedImage rc = test.process(ImageIO.read(new File("./src/main/resources/test_files/00_USA_ROADTRIP.png")));
 
             ImageIO.write(rc, "png", new File("./src/main/resources/test_files/test.png"));
         } catch (IOException e) {
